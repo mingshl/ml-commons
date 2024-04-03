2b1c03d68ceaf11cb8dfd824340bb5a74555f620
Mingshi Liu <mingshl@amazon.com>
2024-04-02 17:26:42 -0700

    add yaml tests and nested objects tests
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/processor/MLInferenceIngestProcessor.java
+++ plugin/src/main/java/org/opensearch/ml/processor/MLInferenceIngestProcessor.java
@@ -8,12 +8,16 @@ import static org.opensearch.ml.common.utils.StringUtils.gson;
 import static org.opensearch.ml.processor.MLModelUtil.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
 import org.opensearch.action.ActionRequest;
 import org.opensearch.client.Client;
 import org.opensearch.core.action.ActionListener;
@@ -29,6 +33,11 @@ import org.opensearch.ml.common.transport.prediction.MLPredictionTaskAction;
 import org.opensearch.script.ScriptService;
 import org.opensearch.script.TemplateScript;
 
+/**
+ * MLInferenceIngestProcessor requires a model_id string to call model inferences
+ * maps fields in document for model input, and maps model inference output to new document fields
+ * this processor also handles dot path notation for nested object( map of array) by rewriting json path accordingly
+ */
 public class MLInferenceIngestProcessor extends AbstractProcessor implements ModelExecutor {
 
     private final MLModelUtil mlModelUtil;
@@ -96,7 +105,9 @@ public class MLInferenceIngestProcessor extends AbstractProcessor implements Mod
         if (i >= round && i != 0) {
             handler.accept(ingestDocument, null);
         } else {
+
             Map<String, String> modelParameters = new HashMap<>();
+
             if (mlModelUtil.getModel_config() != null) {
                 modelParameters.putAll(mlModelUtil.getModel_config());
             }
@@ -112,27 +123,28 @@ public class MLInferenceIngestProcessor extends AbstractProcessor implements Mod
                 for (Map.Entry<String, String> entry : inputMapping.entrySet()) {
                     String originalFieldName = entry.getKey();
                     String ModelInputFieldName = entry.getValue();
-
                     getMappedModelInputFromDocuments(ingestDocument, modelParameters, originalFieldName, ModelInputFieldName);
                 }
             }
 
-            ActionRequest request = getRemoteModelInferenceResult(modelParameters, mlModelUtil.getModel_id());
+            ActionRequest request = getRemoteModelInferenceRequest(modelParameters, mlModelUtil.getModel_id());
 
             client.execute(MLPredictionTaskAction.INSTANCE, request, new ActionListener<>() {
 
                 @Override
                 public void onResponse(MLTaskResponse mlTaskResponse) {
                     ModelTensorOutput modelTensorOutput = (ModelTensorOutput) mlTaskResponse.getOutput();
-
+                    int mini_batch_num = 0;
                     if (process_output == null || process_output.isEmpty()) {
-                        appendFieldValue(modelTensorOutput, null, DEFAULT_OUTPUT_FIELD_NAME, ingestDocument);
+                        appendFieldValue(modelTensorOutput, null, DEFAULT_OUTPUT_FIELD_NAME, ingestDocument, mini_batch_num);
                     } else {
                         Map<String, String> outputMapping = process_output.get(i);
+
                         for (Map.Entry<String, String> entry : outputMapping.entrySet()) {
                             String originalModelOutputFieldName = entry.getKey(); // response
                             String newModelOutputFieldName = entry.getValue(); // inference_result
-                            appendFieldValue(modelTensorOutput, originalModelOutputFieldName, newModelOutputFieldName, ingestDocument);
+                            appendFieldValue(modelTensorOutput, originalModelOutputFieldName, newModelOutputFieldName, ingestDocument,mini_batch_num);
+                            mini_batch_num ++;
                         }
                     }
                     process_predictions(ingestDocument, handler, process_input, process_output, i + 1, round);
@@ -157,33 +169,80 @@ public class MLInferenceIngestProcessor extends AbstractProcessor implements Mod
         String originalFieldPath = getFieldPath(ingestDocument, originalFieldName);
         if (originalFieldPath != null) {
             Object originalFieldValue = ingestDocument.getFieldValue(originalFieldPath, Object.class);
-            String originalFieldValueAsString = getModelInputFieldValue(originalFieldValue);
-            modelParameters.put(ModelInputFieldName, originalFieldValueAsString);
+            String originalFieldValueAsString = toString(originalFieldValue);
+            updateModelParameters(ModelInputFieldName, originalFieldValueAsString,modelParameters);
+        }
+        //check for nested array
+        else {
+            if (originalFieldName.contains(".")) {
+
+                Map<String, Object> sourceObject = ingestDocument.getSourceAndMetadata();
+                Configuration suppressExceptionConfiguration = Configuration.builder().options(Option.SUPPRESS_EXCEPTIONS,Option.DEFAULT_PATH_LEAF_TO_NULL,Option.ALWAYS_RETURN_LIST).build();
+                String rewriteDotPathForNestedObject = findDotPathForNestedObject(sourceObject,originalFieldName);
+                ArrayList<Object>  fieldValueList =JsonPath.using(suppressExceptionConfiguration).parse(sourceObject).read(rewriteDotPathForNestedObject);
+
+                String originalFieldValueAsString = toString(fieldValueList);
+                updateModelParameters(ModelInputFieldName, originalFieldValueAsString,modelParameters);
+            }
         }
     }
 
-    private String getFieldPath(IngestDocument ingestDocument, String originalFieldName) {
-        TemplateScript.Factory originalField = ConfigurationUtils.compileTemplate(TYPE, tag, "field", originalFieldName, scriptService);
-        String originalFieldPath = ingestDocument.renderTemplate(originalField);
-        final boolean fieldPathIsNullOrEmpty = Strings.isNullOrEmpty(originalFieldPath);
-        if (fieldPathIsNullOrEmpty || !ingestDocument.hasField(originalFieldPath, true)) {
-            if (ignoreMissing) {
-                return null;
-            } else if (fieldPathIsNullOrEmpty) {
-                throw new IllegalArgumentException("field name in input_map [ " + originalFieldPath + "] cannot be null nor empty");
+
+    /**
+     * support multiple document fields to map to the same model input fields,
+     * check if the key existed, then append to existed value to be a list.
+     */
+    private void updateModelParameters(String ModelInputFieldName, String originalFieldValueAsString, Map<String, String> modelParameters){
+
+        // Check if the key already exists in the map
+        if (modelParameters.containsKey(ModelInputFieldName)) {
+            Object existingValue = modelParameters.get(ModelInputFieldName);
+            List<Object> updatedList;
+
+            // If the existing value is not a List, create a new List and add the existing value
+            if (!(existingValue instanceof List)) {
+                updatedList = new ArrayList<>();
+                updatedList.add(existingValue);
             } else {
-                throw new IllegalArgumentException("field name in input_map: [" + originalFieldPath + "] doesn't exist");
+                // If the existing value is already a List, cast it appropriately
+                updatedList = (List<Object>) existingValue;
             }
+
+            // Add the new value to the list
+            updatedList.add(originalFieldValueAsString);
+
+            // Update the map with the modified list
+            modelParameters.put(ModelInputFieldName, toString(updatedList));
+        } else {
+            // If the key does not exist, simply put the value in the map
+            modelParameters.put(ModelInputFieldName, originalFieldValueAsString);
         }
-        return originalFieldPath;
+
     }
+
+    private String getFieldPath(IngestDocument ingestDocument, String originalFieldName) {
+        final boolean fieldPathIsNullOrEmpty = Strings.isNullOrEmpty(originalFieldName);
+
+        if (fieldPathIsNullOrEmpty)  {
+            return null;
+        }
+        final boolean hasFieldPath = ingestDocument.hasField(originalFieldName, true);
+        if ( !hasFieldPath) {
+            return null;
+        }
+        return originalFieldName;
+    }
+
+    /**
+     * appendFieldValue to the ingestDocument without changing the source
+     */
 
     private void appendFieldValue(
         ModelTensorOutput modelTensorOutput,
         String originalModelOutputFieldName,
         String newModelOutputFieldName,
-        IngestDocument ingestDocument
-    ) {
+        IngestDocument ingestDocument,
+        int mini_batch_num) {
         Object modelOutputValue = null;
         try {
             modelOutputValue = getModelOutputField(modelTensorOutput, originalModelOutputFieldName, ignoreMissing);
@@ -192,11 +251,38 @@ public class MLInferenceIngestProcessor extends AbstractProcessor implements Mod
                 throw new IllegalArgumentException("model inference output can not find field name: " + originalModelOutputFieldName, e);
             }
         }
+
+        List<String> dotPathsInArray = writeNewDotPathForNestedObject(ingestDocument.getSourceAndMetadata(),newModelOutputFieldName);
+
+        if (dotPathsInArray.size()==1) {
         ValueSource ingestValue = ValueSource.wrap(modelOutputValue, scriptService);
         TemplateScript.Factory ingestField = ConfigurationUtils
             .compileTemplate(TYPE, tag, newModelOutputFieldName, newModelOutputFieldName, scriptService);
         ingestDocument.appendFieldValue(ingestField, ingestValue, false);
-    }
+        }
+        else{
+            if (modelOutputValue instanceof ArrayList){
+
+                ArrayList<?> modelOutputValueArray = (ArrayList<?>) modelOutputValue;
+                int mini_batch_size = modelOutputValueArray.size()/(mini_batch_num+1);
+                ArrayList<?> batchModelOutput = new ArrayList<>(modelOutputValueArray.subList(mini_batch_num * mini_batch_size, (mini_batch_num + 1) * mini_batch_size));
+                //check length to be the same TODO throw exception based on ignoreFailure
+                assert dotPathsInArray.size() == batchModelOutput.size();
+                // Iterate over dotPathInArray
+                for (int i = 0; i < dotPathsInArray.size(); i++) {
+                    String dotPathInArray = dotPathsInArray.get(i);
+                    Object modelOutputValueInArray = batchModelOutput.get(i);
+                    ValueSource ingestValue = ValueSource.wrap(modelOutputValueInArray, scriptService);
+                    TemplateScript.Factory ingestField = ConfigurationUtils
+                            .compileTemplate(TYPE, tag, dotPathInArray, dotPathInArray, scriptService);
+                    ingestDocument.appendFieldValue(ingestField, ingestValue, false);
+                }
+        }
+            else{
+                throw new IllegalArgumentException("model output is not an array, cannot assign to array in documents.");
+            }
+
+    }}
 
     @Override
     public String getType() {
